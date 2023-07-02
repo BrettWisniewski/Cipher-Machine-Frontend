0e561dfb4cee4ee4f812b9b4f410e4c79f46204e
Brett Wisniewski <bpw10va@gmail.com>
2023-07-01 18:46:18 -0600

    added working communication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
 
 function App() {
+  // add a state to determine cipher requests
+  const [backendData, setBackendData] = useState([{}])
+  const [cipheredSentence, setCipheredSentence] = useState("")
+  
+
+  useEffect(() => {
+    fetch('/api').then
+    (res => res.json()).then
+    (data => setBackendData(data))
+  }, [])
+
+  // create onSubmit for the form that sends the input to the backend
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const sentence = e.target.sentence.value
+    console.log(sentence)
+    fetch('/api').then(res => res.json()).then(data => console.log(data)) }
+
+    const handlePost = (e) => {
+      e.preventDefault()
+      const sentence = e.target.sentence.value
+      console.log(sentence)
+      fetch('/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({sentence: sentence})
+      }).then(res => res.json()).then(data => console.log(data))
+    }
+  // create function that on press, sends the sentence hello to the backend
+
+  const handleGet = (e) => {
+    e.preventDefault()
+    fetch('/api').then(res => res.json()).then(data => setCipheredSentence(data))
+  }
+
+  const logData = () => () => {
+    console.log("THIS IS THE DATA")
+    fetch('/caesar').then(res => res.json()).then(data => setCipheredSentence(data.sentence))
+    // what this does is bascially res is data and data is the json
+    
+    
+   
+  }
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Backend Data</h1>
+   {typeof backendData.users !== 'undefined' ? backendData.users.map(user => <div key={user}>{user}</div>) : null}
+
+    {/* create input form to send to the backend that consists of a ciphered sentence */}
+    {/* <form action="/api" method="post" >
+      <label for="sentence">Enter a sentence to be ciphered:</label><br></br>
+      <input type="text" id="sentence" name="sentence" value="Enter a sentence to be ciphered"></input><br></br>
+      <input type="submit" value="Submit"></input>
+    </form> */}
+
+    {/* Make a hard coded post request on the press of a button that sends the sentences RPTHPG */}
+    <button onClick = {logData()}> Send RPTHPG to the backend</button>
+    <h1> {cipheredSentence}</h1>
+
+  
+
     </div>
-  );
+
+  )
 }
 
-export default App;
+
+
+
+
+export default App
